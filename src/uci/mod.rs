5700dab30befcc4670d414655136c97299f35d7d
urisinger <urisig2007@gmail.com>
2024-04-10 16:45:03 +0300

    some more uci work

--- src/uci/mod.rs
+++ src/uci/mod.rs
@@ -1,21 +1,24 @@
 use std::io::{BufRead, Write};
 
 use vampirc_uci::{
-    parse_one, UciMessage, UciMove, UciPiece, UciSearchControl, UciSquare, UciTimeControl,
-};
-
-use crate::board::{
-    piece::{Piece, PieceType},
-    r#move::{Move, MoveType},
+    parse_one, UciFen, UciMessage, UciMove, UciPiece, UciSearchControl, UciSquare, UciTimeControl,
 };
 
 pub trait Engine {
+    fn id() -> (Option<String>, Option<String>) {
+        (None, None)
+    }
+
     fn best_move<W: Write>(
         &mut self,
         writer: &W,
         time_control: Option<UciTimeControl>,
         search_control: Option<UciSearchControl>,
-    ) -> Move;
+    ) -> UciMove;
+
+    fn new_game(&mut self);
+
+    fn set_pos(&mut self, fen: &str, moves: Vec<UciMove>);
 
     fn stop(&self) -> bool {
         false
@@ -29,46 +32,31 @@ struct UciConnection<R: BufRead, W: Write + Send + Sync, E: Engine> {
     engine: E,
 }
 
-fn move_to_uci(r#move: Move) -> UciMove {
-    let promotion = if r#move.move_type() == MoveType::Promote {
-        Some(match r#move.piece().get_type() {
-            PieceType::Pawn => UciPiece::Pawn,
-            PieceType::Knight => UciPiece::Knight,
-            PieceType::Bishop => UciPiece::Bishop,
-            PieceType::Rook => UciPiece::Rook,
-            PieceType::Queen => UciPiece::Queen,
-            PieceType::King => UciPiece::King,
-            PieceType::Empty => panic!("Piece should not be emptry"),
-        })
-    } else {
-        None
-    };
-
-    let from = r#move.from() as u8;
-    let to = r#move.to() as u8;
-    UciMove {
-        from: UciSquare::from((b'a' + (from % 8)) as char, from / 8),
-        to: UciSquare::from((b'a' + (to % 8)) as char, to / 8),
-        promotion,
-    }
-}
-
 impl<R: BufRead, W: Write + Send + Sync, E: Engine> UciConnection<R, W, E> {
+    pub fn new(reader: R, writer: W, engine: E) -> Self {
+        Self { reader, writer, engine }
+    }
+
     pub fn run(mut self) {
         for line in self.reader.lines() {
             match parse_one(&line.unwrap()) {
                 UciMessage::Uci => {
                     self.writer.write_fmt(format_args!("{}", UciMessage::UciOk)).unwrap();
+
+                    let id = E::id();
+
+                    self.writer
+                        .write_fmt(format_args!("{}", UciMessage::Id { name: id.0, author: id.1 }))
+                        .unwrap();
                 }
                 UciMessage::IsReady => {
+                    self.engine.stop();
+
                     self.writer.write_fmt(format_args!("{}", UciMessage::ReadyOk)).unwrap();
                 }
                 UciMessage::Go { time_control, search_control } => {
-                    let best_move = move_to_uci(self.engine.best_move(
-                        &self.writer,
-                        time_control,
-                        search_control,
-                    ));
+                    let best_move =
+                        self.engine.best_move(&self.writer, time_control, search_control);
 
                     self.writer
                         .write_fmt(format_args!(
@@ -76,6 +64,19 @@ impl<R: BufRead, W: Write + Send + Sync, E: Engine> UciConnection<R, W, E> {
                             UciMessage::BestMove { best_move, ponder: None }
                         ))
                         .unwrap();
+                }
+                UciMessage::Position { startpos, fen, moves } => {
+                    let fen = if startpos || fen.is_none() {
+                        "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
+                    } else {
+                        fen.as_ref().unwrap().0.as_str()
+                    };
+
+                    self.engine.set_pos(fen, moves);
+                }
+                UciMessage::UciNewGame => {
+                    self.engine.new_game();
+                    self.writer.write_fmt(format_args!("{}", UciMessage::ReadyOk)).unwrap();
                 }
                 UciMessage::Stop => {
                     self.engine.stop();
